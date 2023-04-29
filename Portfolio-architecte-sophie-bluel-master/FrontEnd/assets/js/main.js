0c55329fb3307b4354dbded2298a7058275fdd62
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-04-29 23:19:46 +0200

    maj
    catégories

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
@@ -7,16 +7,19 @@ fetch("http://localhost:5678/api/works")
             const addElt = document.createElement("figure");
             const addImg = document.createElement("img");
             const addFigc = document.createElement("figcaption");
-            
+            const idImg = data[i].category.name;
+
+
             addImg.src = data[i].imageUrl;
             addImg.alt = data[i].title;
             addFigc.innerHTML = data[i].title;
+            addElt.setAttribute('id',idImg);
+            
 
             galerie.appendChild(addElt);
             addElt.appendChild(addImg);
             addElt.appendChild(addFigc);
-        }
-        
+        }    
     });
 
     // partie btns
@@ -39,22 +42,33 @@ fetch("http://localhost:5678/api/works")
  // Objets
 
       const btnObj = document.querySelector('.Objets');
-      function Objets() {  
 
-          const figObj = document.querySelectorAll('.gallery figure');
-          figObj.forEach(figObj => figObj.classList.remove('block'));
-          figObj.forEach(figObj => figObj.classList.add('none'));
+      function Objets() { 
+        const figures = document.querySelectorAll('.gallery figure');
+        figures.forEach(figure => figure.classList.add('none'));
+
+        
+        const idObjet = document.querySelectorAll('#Objets');
+        idObjet.forEach(idObjet => idObjet.classList.remove('none'));
+        idObjet.forEach(idObjet => idObjet.classList.add('block'));
+        console.log(idObjet);
+
       }
       btnObj.addEventListener('click', Objets);
 
     //   appartements
 
     const btnAppart = document.querySelector('.Appartements');
+
     function appart() {
-      const figures = document.querySelectorAll('.gallery figure');
-      figures.forEach(figure => figure.classList.remove('none'));
-      figures.forEach(figure => figure.classList.add('block'));
-      console.log(data);
+        const figures = document.querySelectorAll('.gallery figure');
+        figures.forEach(figure => figure.classList.add('none'));
+
+        
+        const idObjet = document.querySelectorAll('#Appartements');
+        idObjet.forEach(idObjet => idObjet.classList.remove('none'));
+        idObjet.forEach(idObjet => idObjet.classList.add('block'));
+        console.log(idObjet);
     }
     btnAppart.addEventListener('click', appart);
 
@@ -62,9 +76,14 @@ fetch("http://localhost:5678/api/works")
 
     const btnHotel = document.querySelector('.hotelResto');
     function hotel() {  
-        const figObj = document.querySelectorAll('.gallery figure');
-        figObj.forEach(figObj => figObj.classList.remove('block'));
-        figObj.forEach(figObj => figObj.classList.add('none'));
+        const figures = document.querySelectorAll('.gallery figure');
+        figures.forEach(figure => figure.classList.add('none'));
+
+        
+        const idObjet = document.querySelectorAll('#Hotels & restaurants');
+        idObjet.forEach(idObjet => idObjet.classList.remove('none'));
+        idObjet.forEach(idObjet => idObjet.classList.add('block'));
+        console.log(idObjet);
     }
     btnHotel.addEventListener('click', hotel);
     
