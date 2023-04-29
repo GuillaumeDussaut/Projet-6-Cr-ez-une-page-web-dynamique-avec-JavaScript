32e1bff301ebd2abc577f1017875d9213fea540e
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-04-29 10:09:59 +0200

    MAJ
    creation des btn catégories

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
@@ -1,21 +1,21 @@
 
 
-let imgElt = document.createElement("img")
-imgElt.src = "http://localhost:5678/images/abajour-tahina1651286843956.png"
-imgElt.alt = "Abajour Tahina"
-// imgElt.width = "100"
+// let imgElt = document.createElement("img")
+// imgElt.src = "http://localhost:5678/images/abajour-tahina1651286843956.png"
+// imgElt.alt = "Abajour Tahina"
+// // imgElt.width = "100"
 
-const introContainer = document.getElementById("introduction")
-introContainer.appendChild(imgElt)
+// const introContainer = document.getElementById("introduction")
+// introContainer.appendChild(imgElt)
 
-console.log(imgElt)
+// console.log(imgElt)
 
-fetch("http://localhost:5678/api/works")
-.then( data => data.json() )
-.then(data => {
-    console.log(data)
-})
-.catch(error => console.log("erreur"))
+// fetch("http://localhost:5678/api/works")
+// .then( data => data.json() )
+// .then(data => {
+//     console.log(data)
+// })
+// .catch(error => console.log("erreur"))
 
 
 
