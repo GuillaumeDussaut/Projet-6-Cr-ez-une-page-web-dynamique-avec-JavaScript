3174d5ce2cedfaa1da05692c9c920c30ba296e54
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-04-29 18:40:56 +0200

    Maj
    tests de catégories

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/main.js
@@ -3,7 +3,6 @@ const galerie = document.getElementById("IDgallery")
 fetch("http://localhost:5678/api/works")
     .then( data => data.json() )
     .then(data => {       
-        console.log(data);
         for (let i = 0; i < data.length; i++){
             const addElt = document.createElement("figure");
             const addImg = document.createElement("img");
@@ -12,7 +11,6 @@ fetch("http://localhost:5678/api/works")
             addImg.src = data[i].imageUrl;
             addImg.alt = data[i].title;
             addFig.innerHTML = data[i].title;
-            addElt.classList.add(data[i].category.id);
 
             galerie.appendChild(addElt);
             addElt.appendChild(addImg);
@@ -21,5 +19,56 @@ fetch("http://localhost:5678/api/works")
         
     });
 
+    // partie btns
+
+    
+fetch("http://localhost:5678/api/works")
+    .then(data => data.json())
+    .then(data => {
+
+// Tous
+      const btnTous = document.querySelector('.Tous');
+      function Tous() {
+        const figures = document.querySelectorAll('.gallery figure');
+        figures.forEach(figure => figure.classList.remove('none'));
+        figures.forEach(figure => figure.classList.add('block'));
+        console.log(data);
+      }
+      btnTous.addEventListener('click', Tous);
+
+ // Objets
+
+      const btnObj = document.querySelector('.Objets');
+      function Objets() {  
+
+          const figObj = document.querySelectorAll('.gallery figure');
+          figObj.forEach(figObj => figObj.classList.remove('block'));
+          figObj.forEach(figObj => figObj.classList.add('none'));
+      }
+      btnObj.addEventListener('click', Objets);
+
+    //   appartements
+
+    const btnAppart = document.querySelector('.Appartements');
+    function appart() {
+      const figures = document.querySelectorAll('.gallery figure');
+      figures.forEach(figure => figure.classList.remove('none'));
+      figures.forEach(figure => figure.classList.add('block'));
+      console.log(data);
+    }
+    btnAppart.addEventListener('click', appart);
+
+    // hotel restaurants 
+
+    const btnHotel = document.querySelector('.hotelResto');
+    function hotel() {  
+        const figObj = document.querySelectorAll('.gallery figure');
+        figObj.forEach(figObj => figObj.classList.remove('block'));
+        figObj.forEach(figObj => figObj.classList.add('none'));
+    }
+    btnHotel.addEventListener('click', hotel);
+    
+});
+   
 
 
